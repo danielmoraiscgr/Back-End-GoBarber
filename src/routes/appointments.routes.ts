39c35701ad35e5ae6b735dc7d5ef62cfae8da53c
Daniel Morais Borges <daniel.morais@bumlai.com.br>
2020-11-21 15:13:10 -0400

    Appointment list and create with database

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentsService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+    const appointments = appointmentsRepository.all();
+
+    return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+    const { provider, date } = request.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment =new CreateAppointmentsService(appointmentsRepository);
+
+    const appointment = createAppointment.execute({ date: parsedDate, provider })
+
+    return response.json(appointment);
+});
+
+export default appointmentsRouter;
