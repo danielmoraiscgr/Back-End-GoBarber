2a6046327eeca95c292ea51d512ae460e9761fc6
Daniel Morais Borges <daniel.morais@bumlai.com.br>
2020-11-24 12:31:52 -0400

    JWT and Authentication route

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret : '12aa3453d2f8b94b693409087895e5b3',
+    expiresIn : '1d',
+  }
+}
--- src/database/migrations/1606153062405-CreateUsers.ts
+++ src/database/migrations/1606153062405-CreateUsers.ts
@@ -1,12 +1,11 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class CreateAppointments1605988479279 implements MigrationInterface {
+export class CreateUsers1606153062405 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: 'appointments',
-                
+                name: 'users',                
                 columns: [
                    {
                        name: 'id',
@@ -16,30 +15,36 @@ export class CreateAppointments1605988479279 implements MigrationInterface {
                        default: 'uuid_generate_v4()',
                    },
                    {
-                       name: 'provider',
+                       name: 'name',
+                       type: 'varchar'
+                   },
+                   {
+                       name: 'email',
                        type: 'varchar',
+                       isUnique: true,
                    },
                    {
-                       name: 'date',
-                       type: 'timestamp with time zone',
+                    name: 'password',
+                    type: 'varchar',
+                    },
+                   {
+                       name: 'created_at',
+                       type: 'timestamp',
+                       default: 'now()',
                    },
                    {
-                    name: 'created_at',
+                    name: 'updated_at',
                     type: 'timestamp',
                     default: 'now()',
-                },
-                {
-                 name: 'updated_at',
-                 type: 'timestamp',
-                 default: 'now()',
                 }
+
                 ]
             })
         )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('appointments');
+        await queryRunner.dropTable('users');
     }
 
 }
